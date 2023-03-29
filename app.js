4ce4117b750388db5df7ba6cd6bcf777bd8f66a0
Chris Wood <wchristopherwood912@gmail.com>
2023-03-29 14:23:48 -0700

    adding middleware

--- app.js
+++ app.js
@@ -13,12 +13,19 @@ app.set('view engine', 'handlebars');
 // Routes
 app.get('/', (req, res) => {
     // set the url of the gif
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245';
     // render the hello-gif view, passing the gifUrl into the view to be displayed
-    res.render('hello-gif', { gifUrl })
+    res.render('hello-gif', { gifUrl });
 });
+
+app.get('/greetings/:name', (req, res) => {
+    //get the name from path
+    const name = req.params.name;
+    // render the greetings view and send the name
+    res.render('greetings', {name});
+})
 
 // Start Server
 app.listen(3000, () => {
-    
+    console.log("running on localhost:3000")
 });
